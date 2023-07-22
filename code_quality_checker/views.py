3a9fbec04a04cf7cf916698486f8407c04eb7492
shadhini <shadhini.jayatilake@gmail.com>
2023-07-22 15:38:28 +0530

    change openAi tool used for code analysis

--- code_quality_checker/views.py
+++ code_quality_checker/views.py
@@ -13,6 +13,8 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 openai.api_key = OPENAI_API_KEY
 
+messages = []
+
 
 @ensure_csrf_cookie
 def code_quality_checker_view(request):
@@ -21,6 +23,8 @@ def code_quality_checker_view(request):
         data = {}
         files = request.FILES.getlist('files[]', None)
         data['msg'] = '<span style="color: green;">File successfully uploaded</span>'
+
+        initialize_openai_context()
 
         file_list = []
         for f in files:
@@ -57,8 +61,31 @@ def get_openai_code_completion_response(prompt):
     return reply
 
 
+def get_openai_completion_response(prompt):
+    messages.append({"role": "user", "content": prompt})
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=messages
+    )
+    reply = chat.choices[0].message.content
+    messages.append({"role": "assistant", "content": reply})
+    print(reply)
+    return reply
+
+
+def initialize_openai_context():
+    # initialize messages
+    global messages
+    messages = [
+        {"role": "system",
+         "content": "You are an AI specialized in examining source code to identify potential issues, errors, "
+                    "security vulnerabilities, and other coding best practices to ensure code quality, "
+                    "maintainability, and security and you help to detect bugs, performance bottlenecks, "
+                    "and adherence to coding standards."},
+    ]
+
+
 def analyse_code(file_content):
     code_query = "Please provide inline code quality comments/corrections for the following code \n" + file_content
-    analysis = get_openai_code_completion_response(code_query)
+    analysis = get_openai_completion_response(code_query)
 
     return analysis
